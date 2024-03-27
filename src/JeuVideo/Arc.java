4ca2b1d80444524707a3e3067155525df039944a
Aodemi <mattisbbeaupre@gmail.com>
2024-03-27 12:11:05 -0400

    final commit

--- src/JeuVideo/Arc.java
+++ src/JeuVideo/Arc.java
@@ -1,8 +1,10 @@
 package JeuVideo;
 
 public class Arc extends Arme{
+    String typeDeCorde = "Sèche";
     public Arc(short dpc) {
         super(dpc);
         this.nom = "Arc";
     }
 }
+//         System.out.println(((Arc)arme).typeDeCorde); Appel la méthode de arc
--- src/JeuVideo/Jeu.java
+++ src/JeuVideo/Jeu.java
@@ -3,24 +3,21 @@ package JeuVideo;
 import java.util.Scanner;
 
 public class Jeu {
-    PNJ[] Boss = {new PNJ("Bowser",(short)10,new Dague((short)40),(short)400),
-                  new PNJ("Ganondorf",(short)500,new Epee((short)400),(short)5000),
-                  new PNJ("Malenia",(short)4000,new Arc((short)6000),(short)10000)
-                };
-    Arme[] armes = {
-            new Arc((short)60),
-            new Epee((short)80),
-            new Masse((short)200),
-            new Dague((short)20),
-            new Arbalete((short)80)
-    };
-
-    PNJ[] Accolyte = {
-            new PNJ("Robin", (short)30, armes[0], (short)120),
-            new PNJ("Hulk", (short)400,armes[2],(short)6000)
-    };
 
     public static void main(String[] args) {
+        Personnage boss = new PNJ("Bowser",(short)40,new Dague((short)40),(short)400);
+        PNJ[] bosses = {new PNJ("Bowser",(short)40,new Dague((short)40),(short)400),
+                new PNJ("Ganondorf",(short)500,new Epee((short)400),(short)5000),
+                new PNJ("Malenia",(short)1000,new Arc((short)6000),(short)10000)
+        };
+        Arme[] armes = {
+                new Arc((short)60),
+                new Epee((short)80),
+                new Masse((short)200),
+                new Dague((short)20),
+                new Arbalete((short)80)
+        };
+
         System.out.println(" Sélectionner votre arme");
         System.out.println(" 1 - Arc");
         System.out.println(" 2 - Épée");
@@ -51,6 +48,37 @@ public class Jeu {
         }
 
         System.out.println(arme);
+
+        Hero perso = new Hero("Vide", (short)1, arme, (short)200, 0);
+
+        public static void combattre(Hero perso, Personnage boss){
+            while(true){
+                while(perso.getPdv() != 0 && boss.getPdv() != 0){
+                    perso.attaquer(boss);
+                    if(boss.getPdv() != 0){
+                        boss.attaquer(perso);
+                    }
+                }
+
+                System.out.println(perso);
+                System.out.println(boss);
+
+                if (perso.getPdv() == 0){
+                    perso.setXp(perso.getXp() + boss.getNiveau()*2);
+                    break;
+                }
+                if (boss.getPdv() == 0){
+                    perso.setXp(boss.getXp() + perso.getNiveau()*8);
+                    break;
+                }
+                boss.setPdv((short)(10 * boss.getNiveau()));
+                perso.setPdv((short)(180 + 20 * perso.getNiveau()));
+            }
+        }
+
+
+
+
     }
 
 }
